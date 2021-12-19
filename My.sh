777e8e585e59f3ea6e84f085c49435438e1dec45
Aภl๏miuภuຮ <51442719+Anlominus@users.noreply.github.com>
2021-12-20 00:49:49 +0200

    Create My.sh

---
+++ My.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Enter the IP address of the Application Server"
+read application_server_ip
+echo "Enter email address to send Monit alerts to"
+read monit_alert_email
+echo "Enter the SMTP host for Monit"
+read monit_smtp_host
+echo "Enter the SMTP port for Monit"
+read monit_smtp_port
+echo "Enter the SMTP username for Monit"
+read monit_smtp_username
+echo "Enter the SMTP password for Monit"
+read monit_smtp_password
