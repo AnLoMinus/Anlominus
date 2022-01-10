f5f84492a068265921405e64e93112e41bdbfe4c
Aภl๏miuภuຮ <51442719+Anlominus@users.noreply.github.com>
2022-01-10 13:46:52 +0200

    Update HacKing.sh

--- HacKing/HacKing.sh
+++ HacKing/HacKing.sh
@@ -56,7 +56,7 @@ echo "       #    |  _  | (_| | (__| . \| | | | | (_| |    #"
 echo "       #    |_| |_|\__,_|\___|_|\_\_|_| |_|\__, |    #"
 echo "       #                                   |___/     #"
 echo -e "$red       ###############################################"
-echo -e "  $red[$green+$red]$cyan Github: https://www.github.com/Anlominus/HacKing $red[$green+$red]$nc"
+echo -e "  $red[$green+$red]$cyan Github: https://www.github.com/Anlominus/HacKing $red[$green+$red]"
 
 }
 
@@ -91,7 +91,7 @@ echo -e "$purple
  ......:.;:::ccllloooodddxxxddddooooollcc::;'c,':......
   .''..cc.''''''',,,,,,,;;;;;,,,,,,,,''''....:.cc..''.
 "
-echo -e "  $red[$green+$red]$cyan Github: https://www.github.com/Anlominus/HacKing $red[$green+$red]$nc"
+echo -e "  $red[$green+$red]$cyan Github: https://www.github.com/Anlominus/HacKing $red[$green+$red]"
 }
 
 ################
@@ -125,107 +125,207 @@ n8="8"
 n9="9"
 n10="10"
 n11="11"
-n12="12"
+n13="13"
+n14="14"
+n15="15"
+n16="16"
+n17="17"
+n18="18"
+
+##################
+#    VTCOLOR     #
+##################
+c0="$red[$green$n0$red]$white"
+c1="$red[$green$n1$red]$white"
+c2="$red[$green$n2$red]$white"
+c3="$red[$green$n3$red]$white"
+c4="$red[$green$n4$red]$white"
+c5="$red[$green$n5$red]$white"
+c6="$red[$green$n6$red]$white"
+c7="$red[$green$n7$red]$white"
+c8="$red[$green$n8$red]$white"
+c9="$red[$green$n9$red]$white"
+c10="$red[$green$n10$red]$white"
+c11="$red[$green$n11$red]$white"
 
 ################
 #    VMenu     #
 ################
-m1="$red[$green$n1$red]$nc$white AnonSurf:$cafe Anonymity surfing $nc"
-m2="$red[$green$n2$red]$nc$white WebHack:$cafe Web Analysis $nc"
-m3="$red[$green$n3$red]$nc$white VulnHack:$cafe Vulnerability Analysis $nc"
-m4="$red[$green$n4$red]$nc$white WireLess:$cafe Wireless Analysis $nc"
-m5="$red[$green$n5$red]$nc$white InfoGath:$cafe Information Gathering $nc"
-m6="$red[$green$n6$red]$nc$white Forensics:$cafe Forensics Analysis $nc"
-m7="$red[$green$n7$red]$nc$white FramXploit:$cafe Exploit Frameworks $nc"
-m8="$red[$green$n8$red]$nc$white RAT:$cafe RemoteAccess $nc"
-m9="$red[$green$n9$red]$nc$white HasHack:$cafe Hashing & Enctyprt Decrypt $nc"
-m10="$red[$green$n10$red]$nc$white PayLoads:$cafe Payloads and more $nc"
-m11="$red[$green$n11$red]$nc$white XInstKits:$cafe Installer Kits $nc"
-m00="$red[$green$n00$red]$nc$white Exit $nc"
+m1="$c1$cyan AnonSurf:$cafe Anonymity surfing "
+m2="$c2$cyan WebHack:$cafe Web Analysis "
+m3="$c3$cyan VulnHack:$cafe Vulnerability Analysis "
+m4="$c4$cyan WireLess:$cafe Wireless Analysis "
+m5="$c5$cyan InfoGath:$cafe Information Gathering "
+m6="$c6$cyan Forensics:$cafe Forensics Analysis "
+m7="$c7$cyan FramXploit:$cafe Exploit Frameworks "
+m8="$c8$cyan RAT:$cafe RemoteAccess "
+m9="$c9$cyan HasHack:$cafe Hashing & Enctyprt Decrypt "
+m10="$c10$cyan PayLoads:$cafe Payloads and more "
+m11="$c11$cyan XInstKits:$cafe Installer Kits "
+m00="$c0 "
 ##################
 #    VMenu_1     #
 ##################
-m1_1="$red[$green$n1$red]$nc$white AnonSurf $nc"
-m1_0="$red[$green$n00$red]$nc$white Exit $nc"
+#  [1] AnonSurf: Anonymity surfing
+m1_1="$c1 AnonSurf "
+m1_0="$c0 Exit "
 ##################
 #    VMenu_2     #
 ##################
-m2_1="$red[$green$n1$red]$nc$white Devices Information $nc"
-m2_2="$red[$green$n2$red]$nc$white Th3inspector $nc"
-m2_3="$red[$green$n3$red]$nc$white Mercury $nc"
-m2_4="$red[$green$n4$red]$nc$white 000000 $nc"
-m2_0="$red[$green$n00$red]$nc$white Exit $nc"
+#  [2] WebHack: Web Analysis
+m2_1="$c1 BlackWidow "
+m2_2="$c2 Blazy "
+m2_3="$c3 Breacher "
+m2_4="$c4 checkURL "
+m2_5="$c3 Cracker-Tool "
+m2_6="$c4 curl "
+m2_7="$c3 dirb-kali "
+m2_8="$c4 dirsearch "
+m2_9="$c3 dotdotpwn "
+m2_10="$c4 ffuf "
+m2_11="$c3 HawkScan "
+m2_12="$c4 insta-hack "
+m2_13="$c3 nikto "
+m2_14="$c4 Sublist3r "
+m2_15="$c3 takeover "
+m2_16="$c4 WebHackersWeapon "
+m2_17="$c3 webkiller "
+m2_18="$c4 XSS "
+m2_0="$c0 Exit "
 ##################
 #    VMenu_3     #
 ##################
-m3_1="$red[$green$n1$red]$nc$white HacKing $nc"
-m3_2="$red[$green$n2$red]$nc$white Lazymux $nc"
-m3_3="$red[$green$n3$red]$nc$white Tool-X $nc"
-m3_0="$red[$green$n00$red]$nc$white Exit $nc"
+#  [3] VulnHack: Vulnerability Analysis
+m3_1="$c1 ChangeMe "
+m3_2="$c2 ChangeMe "
+m3_3="$c3 ChangeMe "
+m3_0="$c0 Exit "
 ##################
 #    VMenu_4     #
 ##################
-m4_1="$red[$green$n1$red]$nc$white HacKing $nc"
-m4_2="$red[$green$n2$red]$nc$white Lazymux $nc"
-m4_3="$red[$green$n3$red]$nc$white Tool-X $nc"
-m4_0="$red[$green$n00$red]$nc$white Exit $nc"
+#  [4] WireLess: Wireless Analysis
+m4_1="$c1 HacKing "
+m4_2="$c2 Lazymux "
+m4_3="$c3 Tool-X "
+m4_0="$c0 Exit "
 ##################
 #    VMenu_5     #
 ##################
-m5_1="$red[$green$n1$red]$nc$white HacKing $nc"
-m5_2="$red[$green$n2$red]$nc$white Lazymux $nc"
-m5_3="$red[$green$n3$red]$nc$white Tool-X $nc"
-m5_4="$red[$green$n4$red]$nc$white DarkArmy $nc"
-m5_5="$red[$green$n5$red]$nc$white Fsociety $nc"
-m5_0="$red[$green$n00$red]$nc$white Exit $nc"
+#  [5] InfoGath: Information Gathering
+m5_1="$c1 HacKing "
+m5_2="$c2 Lazymux "
+m5_3="$c3 Tool-X "
+m5_4="$c4 DarkArmy "
+m5_5="$c5 Fsociety "
+m5_0="$c0 Exit "
 ##################
-#    VMenu_1     #
+#    VMenu_6     #
 ##################
-m1_1="$red[$green$n1$red]$nc$white Anonymity $nc"
-m1_2="$red[$green$n2$red]$nc$white Lazymux $nc"
-m1_3="$red[$green$n3$red]$nc$white Tool-X $nc"
-m1_0="$red[$green$n00$red]$nc$white Exit $nc"
+#  [6] Forensics: Forensics Analysis
+m6_1="$c1 Anonymity "
+m6_2="$c2 Lazymux "
+m6_3="$c3 Tool-X "
+m6_0="$c0 Exit "
 ##################
-#    VMenu_2     #
+#    VMenu_7     #
 ##################
-m2_1="$red[$green$n1$red]$nc$white Devices Information $nc"
-m2_2="$red[$green$n2$red]$nc$white Th3inspector $nc"
-m2_3="$red[$green$n3$red]$nc$white Mercury $nc"
-m2_4="$red[$green$n4$red]$nc$white 000000 $nc"
-m2_0="$red[$green$n00$red]$nc$white Exit $nc"
+#  [7] FramXploit: Exploit Frameworks
+m7_1="$c1 Devices Information "
+m7_2="$c2 Th3inspector "
+m7_3="$c3 Mercury "
+m7_4="$c4 000000 "
+m7_0="$c0 Exit "
 ##################
-#    VMenu_3     #
+#    VMenu_8     #
 ##################
-m3_1="$red[$green$n1$red]$nc$white HacKing $nc"
-m3_2="$red[$green$n2$red]$nc$white Lazymux $nc"
-m3_3="$red[$green$n3$red]$nc$white Tool-X $nc"
-m3_0="$red[$green$n00$red]$nc$white Exit $nc"
+#  [8] RAT: RemoteAccess
+m8_1="$c1 Ciphey "
+m8_2="$c2 haiti "
+m8_3="$c3 Hash-Buster "
+m8_0="$c0 Exit "
 ##################
-#    VMenu_4     #
+#    VMenu_9     #
 ##################
-m4_1="$red[$green$n1$red]$nc$white HacKing $nc"
-m4_2="$red[$green$n2$red]$nc$white Lazymux $nc"
-m4_3="$red[$green$n3$red]$nc$white Tool-X $nc"
-m4_0="$red[$green$n00$red]$nc$white Exit $nc"
-##################
-#    VMenu_5     #
-##################
-m5_1="$red[$green$n1$red]$nc$white HacKing $nc"
-m5_2="$red[$green$n2$red]$nc$white Lazymux $nc"
-m5_3="$red[$green$n3$red]$nc$white Tool-X $nc"
-m5_4="$red[$green$n4$red]$nc$white DarkArmy $nc"
-m5_5="$red[$green$n5$red]$nc$white Fsociety $nc"
-m5_0="$red[$green$n00$red]$nc$white Exit $nc"
+#  [9] HasHack: Hashing & Enctyprt Decrypt
+m9_1="$c1 Ciphey "
+m9_2="$c2 haiti "
+m9_3="$c3 Hash-Buster "
+m9_4="$c4 hashids "
+m9_5="$c5 imagehash "
+m9_6="$c6 Name-That-Hash "
+m9_7="$c7 Search-That-Hash "
+m9_8="$c8 onemore "
+m9_9="$c9 onemore "
+m9_0="$c0 Exit "
+###################
+#    VMenu_10     #
+###################
+#  [10] PayLoads: Payloads and more
+m10_1="$c1 HacKing "
+m10_2="$c2 Lazymux "
+m10_3="$c3 Tool-X "
+m10_4="$c4 DarkArmy "
+m10_5="$c5 Fsociety "
+m10_0="$c0 Exit "
+###################
+#    VMenu_11     #
+###################
+#  [11] XInstKits: Installer Kits
+m11_1="$c1 HacKing "
+m11_2="$c2 Lazymux "
+m11_3="$c3 Tool-X $snc"
+m11_4="$c4 DarkArmy "
+m11_5="$c5 Fsociety "
+m11_0="$c0 Exit "
+###################
+#    VMenu_12     #
+###################
+m12_1="$c1 Anonymity "
+m12_2="$c2 Lazymux "
+m12_3="$c3 Tool-X "
+m12_0="$c0 Exit "
+###################
+#    VMenu_13     #
+###################
+m13_1="$c1 Devices Information "
+m13_2="$c2 Th3inspector "
+m13_3="$c3 Mercury "
+m13_4="$c4 000000 "
+m13_0="$c0 Exit "
+###################
+#    VMenu_14     #
+###################
+m14_1="$c1 HacKing "
+m14_2="$c2 Lazymux "
+m14_3="$c3 Tool-X "
+m14_0="$c0 Exit "
+###################
+#    VMenu_15     #
+###################
+m15_1="$c1 HacKing "
+m15_2="$c2 Lazymux "
+m15_3="$c3 Tool-X "
+m15_0="$c0 Exit "
+###################
+#    VMenu_16     #
+###################
+m16_1="$c1 HacKing "
+m16_2="$c2 Lazymux "
+m16_3="$c3 Tool-X "
+m16_4="$c4 DarkArmy "
+m16_5="$c5 Fsociety "
+m16_0="$c0 Exit "
 
+###################
 
 ################
 #    FMenu     #
 ################
 function Menu() {
 	echo -e "\n
-        $red[$green#############$red]$nc$white $nc
-        $red[$green#$cyan Main Menu $green#$red]$nc$white $nc
-        $red[$green#############$red]$nc$white $nc\n
+        $red[$green#############$red]$white
+        $red[$green#$cyan Main Menu $green#$red]$white
+        $red[$green#############$red]$white \n
         $m1
         $m2
         $m3
@@ -376,9 +476,9 @@ function Menu() {
 ################
 function Menu_1() {
 	echo -e "\n
-        $red[$green#############$red]$nc$white $nc
-        $red[$green#$cyan Anonymity $green#$red]$nc$white $nc
-        $red[$green#############$red]$nc$white $nc\n
+        $red[$green#############$red]$white
+        $red[$green#$cyan Anonymity $green#$red]$white
+        $red[$green#############$red]$white \n
 \n      $m00
 "
   while :
@@ -452,9 +552,9 @@ function Menu_1() {
 function Menu_2(){
   Banner
   echo -e "\n
-  $red[$green#########################$red]$nc$white $nc
-  $red[$green#$cyan Information Gathering $green#$red]$nc$white $nc
-  $red[$green#########################$red]$nc$white $nc\n
+  $red[$green#########################$red]$white
+  $red[$green#$cyan Information Gathering $green#$red]$white
+  $red[$green#########################$red]$white \n
         $m2_1
         $m2_2
         $m2_3
@@ -468,9 +568,9 @@ function Menu_2(){
     case $INPUT_STRING in
     menu)
       ;;
-    1)
-      echo "$(clear)"
-      bash Infogath/SystInfo.sh
+  	1)
+    echo "$(clear)"
+    echo -e "$m2_1"
     ;;
     2)
       echo "$(clear)"
@@ -501,9 +601,9 @@ function Menu_2(){
 function Menu_3(){
   Banner
   echo -e "\n
-  $red[$green#################$red]$nc$white $nc
-  $red[$green#$cyan InstKits Menu $green#$red]$nc$white $nc
-  $red[$green#################$red]$nc$white $nc\n
+  $red[$green#################$red]$white
+  $red[$green#$cyan InstKits Menu $green#$red]$white
+  $red[$green#################$red]$white \n
         $m5_1
         $m5_2
         $m5_3
@@ -549,9 +649,9 @@ function Menu_3(){
 function Menu_4(){
   Banner
   echo -e "\n
-  $red[$green#################$red]$nc$white $nc
-  $red[$green#$cyan InstKits Menu $green#$red]$nc$white $nc
-  $red[$green#################$red]$nc$white $nc\n
+  $red[$green#################$red]$white
+  $red[$green#$cyan InstKits Menu $green#$red]$white
+  $red[$green#################$red]$white \n
         $m5_1
         $m5_2
         $m5_3
@@ -595,9 +695,9 @@ function Menu_4(){
 function Menu_5(){
   Banner
   echo -e "\n
-  $red[$green##################$red]$nc$white $nc
-  $red[$green#$cyan Installer Kits $green#$red]$nc$white $nc
-  $red[$green##################$red]$nc$white $nc\n
+  $red[$green##################$red]$white
+  $red[$green#$cyan Installer Kits $green#$red]$white
+  $red[$green##################$red]$white \n
         $m5_1
         $m5_2
         $m5_3
